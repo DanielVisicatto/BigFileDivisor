cdd54677d22db9f6be630bbb39d8a28b107436bf
Daniel Visicatto <dpfreestyle@gmail.com>
2023-06-21 12:50:27 -0300

    Adicionado caixa de texto para qu o usuário possa determinar quantidade de linhas em cada novo arquivo e adicionado suporte para arquivos .txt e .docx

--- BigFileDivisor/Form1.Designer.cs
+++ BigFileDivisor/Form1.Designer.cs
@@ -31,11 +31,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(58, 333);
+            this.button1.Location = new System.Drawing.Point(407, 245);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(270, 23);
             this.button1.TabIndex = 0;
@@ -43,16 +44,30 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.textBox1.Location = new System.Drawing.Point(250, 248);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "numb. of rows";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.UseWaitCursor = true;
+            this.textBox1.MouseEnter += new System.EventHandler(this.textBox1_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(730, 391);
+            this.ClientSize = new System.Drawing.Size(690, 280);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "BigFile Divisor 1.0";
+            this.Text = "BigFile Divisor 1.1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -60,6 +75,7 @@
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- BigFileDivisor/Form1.cs
+++ BigFileDivisor/Form1.cs
@@ -1,15 +1,25 @@
 ﻿using ClosedXML.Excel;
 using System;
-using System.Linq;
+using System.IO;
 using System.Windows.Forms;
+using Xceed.Words.NET;
 
 namespace BigFileDivisor
 {
     public partial class Form1 : Form
     {
+        private const string DefaultTextBoxText = "Numb. of rows";
         public Form1()
         {
             InitializeComponent();
+            textBox1.Text = DefaultTextBoxText;
+            textBox1.ForeColor = System.Drawing.SystemColors.GrayText;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -17,44 +27,116 @@ namespace BigFileDivisor
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 string arquivoOrigem = openFileDialog1.FileName;
-                int linhasPorArquivo = 10;
+                int linhasPorArquivo = int.Parse(textBox1.Text);
 
-                SepararArquivo(arquivoOrigem, linhasPorArquivo);
+                if (Path.GetExtension(arquivoOrigem) == ".xlsx")
+                {
+                    SepararArquivoExcel(arquivoOrigem, linhasPorArquivo);
+                }
+                else if (Path.GetExtension(arquivoOrigem) == ".txt")
+                {
+                    SepararArquivoTexto(arquivoOrigem, linhasPorArquivo);
+                }
+                else if (Path.GetExtension(arquivoOrigem) == ".docx")
+                {
+                    SepararArquivoDocx(arquivoOrigem, linhasPorArquivo);
+                }
+                else
+                {
+                    MessageBox.Show("Formato de arquivo não suportado. Por favor, selecione um arquivo .xlsx, .txt ou .docx.");
+                }
             }
         }
 
-        private void SepararArquivo(string arquivoOrigem, int linhasPorArquivo)
+        private void SepararArquivoExcel(string arquivoOrigem, int linhasPorArquivo)
         {
-            using (var workbookOrigem = new XLWorkbook(arquivoOrigem))
+            using (var workbook = new XLWorkbook(arquivoOrigem))
             {
-                var worksheetOrigem = workbookOrigem.Worksheet(1);
-                int totalLinhas = worksheetOrigem.RowsUsed().Count();
-                int totalArquivos = totalLinhas / linhasPorArquivo;
+                var worksheet = workbook.Worksheet(1);
+                var range = worksheet.RangeUsed();
+
+                int totalLinhas = range.RowCount();
+                int totalArquivos = (int)Math.Ceiling((double)totalLinhas / linhasPorArquivo);
 
                 for (int i = 0; i < totalArquivos; i++)
                 {
                     string nomeArquivo = $"arquivo_{i + 1}.xlsx";
 
-                    using (var workbookDestino = new XLWorkbook())
+                    using (var newWorkbook = new XLWorkbook())
                     {
-                        var worksheetDestino = workbookDestino.Worksheets.Add("Sheet1");
+                        var newWorksheet = newWorkbook.Worksheets.Add("Sheet1");
 
-                        int inicio = i * linhasPorArquivo;
-                        int fim = inicio + linhasPorArquivo;
+                        int inicio = i * linhasPorArquivo + 1;
+                        int fim = Math.Min(inicio + linhasPorArquivo - 1, totalLinhas);
 
-                        for (int linha = inicio; linha < fim; linha++)
-                        {
-                            var linhaOrigem = worksheetOrigem.Row(linha + 1);
-                            var linhaDestino = worksheetDestino.Row(linha - inicio + 1);
+                        var rangeCopy = range.Range(inicio, 1, fim, range.ColumnCount());
+                        rangeCopy.CopyTo(newWorksheet.Cell(1, 1));
 
-                            linhaOrigem.CopyTo(linhaDestino);
-                        }
-
-                        workbookDestino.SaveAs(nomeArquivo);
+                        newWorkbook.SaveAs(nomeArquivo);
                     }
                 }
 
                 MessageBox.Show($"{totalArquivos} arquivos Excel foram gerados.");
+            }
+        }
+
+        private void SepararArquivoTexto(string arquivoOrigem, int linhasPorArquivo)
+        {
+            string[] linhas = File.ReadAllLines(arquivoOrigem);
+            int totalLinhas = linhas.Length;
+            int totalArquivos = (int)Math.Ceiling((double)totalLinhas / linhasPorArquivo);
+
+            for (int i = 0; i < totalArquivos; i++)
+            {
+                string nomeArquivo = $"arquivo_{i + 1}.txt";
+
+                int inicio = i * linhasPorArquivo;
+                int fim = Math.Min(inicio + linhasPorArquivo, totalLinhas);
+
+                string[] linhasSelecionadas = new string[fim - inicio];
+                Array.Copy(linhas, inicio, linhasSelecionadas, 0, fim - inicio);
+
+                File.WriteAllLines(nomeArquivo, linhasSelecionadas);
+            }
+
+            MessageBox.Show($"{totalArquivos} arquivos de texto foram gerados.");
+        }
+
+        private void SepararArquivoDocx(string arquivoOrigem, int linhasPorArquivo)
+        {
+            textBox1.Click += (sender, e) =>
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            };
+
+            using (var document = DocX.Load(arquivoOrigem))
+            {
+                var paragraphs = document.Paragraphs;
+
+                int totalParagrafos = paragraphs.Count;
+                int totalArquivos = (int)Math.Ceiling((double)totalParagrafos / linhasPorArquivo);
+
+                for (int i = 0; i < totalArquivos; i++)
+                {
+                    string nomeArquivo = $"arquivo_{i + 1}.docx";
+
+                    using (var newDocument = DocX.Create(nomeArquivo))
+                    {
+                        int inicio = i * linhasPorArquivo;
+                        int fim = Math.Min(inicio + linhasPorArquivo, totalParagrafos);
+
+                        for (int j = inicio; j < fim; j++)
+                        {
+                            var paragraph = paragraphs[j];
+                            newDocument.InsertParagraph(paragraph);
+                        }
+
+                        newDocument.Save();
+                    }
+                }
+
+                MessageBox.Show($"{totalArquivos} arquivos DOCX foram gerados.");
             }
         }
     }
